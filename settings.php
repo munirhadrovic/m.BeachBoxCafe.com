8d59c2b246f839e6a352328015763f0c814a4513
novicaa <novicamiketic@gmail.com>
2015-04-23 11:20:56 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,12 @@
+
+<?php
+$adresa[0]="9020 Gulf Shore Drive,Naples, FL 34108"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[0]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[0]="Naples";
+$telephone[0]=2393010950;// Broj koji se poziva na dugme CALL US
+
+$companynumber=1;
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
